c241c837c0a4987c8d1d4df65dbdaa47d8063db4
1463353697 <1463353697@qq.com>
2019-12-12 19:43:51 +0800

    2/3

--- thebusitem/src/components/theFlow.vue
+++ thebusitem/src/components/theFlow.vue
@@ -9,6 +9,8 @@
                     <option value="3">3月份</option>
                     <option value="4">4月份</option>
                     <option value="5">5月份</option>
+                    <option value="6">6月份</option>
+                    <option value="7">7月份</option>
                 </select>
             </div>
             <div class="flow-charts">
@@ -28,6 +30,7 @@ export default {
     data(){
         return{
             
+            
 
         }
     },
@@ -42,17 +45,102 @@ export default {
         monthChoose(){
             var monthSelect = document.getElementById('month-select');
             var monthIndex = monthSelect.selectedIndex;
+
+            //获得选择的月份的值
             var theMonth = monthSelect.options[monthIndex].value;
             
-            var theMonthFlow = []
-            //这里在对月份数据进行筛选，但遇到了两个大问题:substring方法无法识别，
-            //还有从父组件中得到的月份数据无法在这里得到
-            flowData.forEach(function(item){
-                var thedate = item.date;
-                // if(thedate.substring(5,6) == this.flowId){
-                //     theMonthFlow.push(item);
-                // }
-            })
+            
+            var theid = this.flowId;
+            console.log(theid);
+
+            //暂时先看id筛选出数据
+            var theIdFlow = [];
+
+            var theFinalFlow = [];
+
+            
+            // 这里在对月份数据进行筛选，但遇到了问题:substring方法无法识别，
+            //为了不耽误进度先这样：先取出符合已选择id线路的数据，然后再根据选择的月份根据每月的天数筛选
+            for(var i =0,len = flowData.length;i<len;i++){
+                if(flowData[i].id == theid){
+                    theIdFlow.push(flowData[i]);
+                    
+                }
+            }
+            console.log(theIdFlow);
+
+            //根据月份再筛选数据
+            if(theMonth == 1){
+                for(var i = 0;i<31;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 2){
+                for(var i = 31;i<59;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 3){
+                for(var i = 59;i<90;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 4){
+                for(var i = 90;i<120;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 5){
+                for(var i = 120;i<151;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 6){
+                for(var i = 151;i<181;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            if(theMonth == 7){
+                for(var i = 181;i<212;i++){
+                    theFinalFlow.push(theIdFlow[i]);
+                }
+            }
+            console.log(theFinalFlow);
+
+
+
+
+
+            
+            // flowData.forEach(function(item){
+            //     var thedate = item.date;   
+            //     // console.log(thedate);
+            //     var thestring = thedate.substring(5,6);
+            //     if(thestring == theMonth && item.id == theid ){
+            //         theMonthFlow.push(item);
+            //     }
+            // });
+            // var test1 = "abc";
+            // var test2 = test1.substring(1,2);
+            // console.log(test2);
+            // var thedatadate = [];
+
+            // for(var i=0;i<flowData.length;i++){
+            //      thedatadate.push(flowData[i].date);
+                
+            // }
+            // // console.log(thedatadate);
+            // // var testMonth = thedatadate[2].substring(5,6);
+            // // console.log(testMonth);
+            // var checkedIndex = [];
+            // for(var i = 0,len=thedatadate.length;i<len;i++){
+            //     var arraydate = thedatadate[3];
+            //     var testMonth = arraydate.substring(5,6);
+            //     if(testMonth == theMonth ){
+            //         checkedIndex.push(i);
+            //     }
+            // }
+
 
         }
     }
