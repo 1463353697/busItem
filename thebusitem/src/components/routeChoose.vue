8fb2c1d6b48a17bbf898c5e10e78103f519356c2
1463353697 <1463353697@qq.com>
2019-12-13 11:18:09 +0800

    add the flow charts

--- thebusitem/src/components/routeChoose.vue
+++ thebusitem/src/components/routeChoose.vue
@@ -144,7 +144,7 @@ export default {
   }
   .station-map {
     width: 100%;
-    height: 700px;
+    height: 300px;
    
   }
 
--- thebusitem/src/components/theFlow.vue
+++ thebusitem/src/components/theFlow.vue
@@ -14,15 +14,19 @@
                 </select>
             </div>
             <div class="flow-charts">
+                <div id="echarts1"></div>
+                <div id="echarts2"></div>
 
             </div>
         </div>
     </div>
     
 </template>
+ 
 <script>
 // 引入客流量数据
 import flowData from '../assets/flowData.json'
+import echarts from '../../echarts.js'
 
 export default {
     props:['flowId'],
@@ -51,12 +55,14 @@ export default {
             
             
             var theid = this.flowId;
-            console.log(theid);
+            
 
             //暂时先看id筛选出数据
             var theIdFlow = [];
-
+            //用来存放筛选出来的符合id和月份的数据
             var theFinalFlow = [];
+            //用来存放符合所有条件的最后的客流量数据数组
+            var monthDays = [];
 
             
             // 这里在对月份数据进行筛选，但遇到了问题:substring方法无法识别，
@@ -67,45 +73,166 @@ export default {
                     
                 }
             }
-            console.log(theIdFlow);
+           
 
             //根据月份再筛选数据
             if(theMonth == 1){
                 for(var i = 0;i<31;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+
             }
             if(theMonth == 2){
                 for(var i = 31;i<59;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
             if(theMonth == 3){
                 for(var i = 59;i<90;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
             if(theMonth == 4){
                 for(var i = 90;i<120;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
             if(theMonth == 5){
                 for(var i = 120;i<151;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
             if(theMonth == 6){
                 for(var i = 151;i<181;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
             if(theMonth == 7){
                 for(var i = 181;i<212;i++){
                     theFinalFlow.push(theIdFlow[i]);
                 }
+                var monthDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
             }
-            console.log(theFinalFlow);
+            //得到最后筛选出来的当月该路线的客流量数据
+            var thePassenger = [];
+            for(var i = 0; i<theFinalFlow.length; i++){
+                thePassenger.push(theFinalFlow[i].passenger);
+            }
+            // console.log(thePassenger);
+            // console.log(theFinalFlow);
+
+            var flowChart1 = echarts.init(document.getElementById('echarts1'));
+            var flowChart2 = echarts.init(document.getElementById('echarts2'));
+
+            var flowOption1 = {
+                color :['#B1D3DD'],
+                tooltip : {
+                    show : true
+                },
+                legend : {
+                    data : ['客流量']
+                },
+                xAxis : [{
+                    type : 'category',
+                    data : monthDays
+                }],
+                yAxis:[{
+                    type : 'value'
+                }],
+                series : [{
+                    "name" : "客流量",
+                    "type" : "bar",
+                    "data" : thePassenger
+                }]
+            };
+
+            flowChart1.setOption(flowOption1);
+
+            //选择了月份之后，需要取得下一个月份的数据
+            var theNextflow = [];
+            var theNextDays = [];
+             if(theMonth == 1){
+                for(var i = 31;i<59;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+
+            }
+            if(theMonth == 2){
+                for(var i = 59;i<90;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            if(theMonth == 3){
+                for(var i = 90;i<120;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            if(theMonth == 4){
+                for(var i = 120;i<151;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            if(theMonth == 5){
+                for(var i = 151;i<181;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            if(theMonth == 6){
+                for(var i = 181;i<212;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            if(theMonth == 7){
+                for(var i = 181;i<212;i++){
+                    theNextflow.push(theIdFlow[i]);
+                }
+                var theNextDays = Array.from(new Array(theFinalFlow.length + 1).keys()).slice(1);
+            }
+            //得到最后筛选出来的当月该路线的客流量数据
+            console.log(theNextflow);
+            var theNextPassenger = [];
+            for(var i = 0; i<theNextflow.length; i++){
+                theNextPassenger.push(theNextflow[i].passenger);
+            }
+             var flowOption2 = {
+                color : ['#96B4D6'],
+                tooltip : {
+                    show : true
+                },
+                legend : {
+                    data : ['下月客流量']
+                },
+                xAxis : [{
+                    type : 'category',
+                    data : theNextDays
+                }],
+                yAxis:[{
+                    type : 'value'
+                }],
+                series : [{
+                    "name" : "下月客流量",
+                    "type" : "bar",
+                    "data" : theNextPassenger
+                }]
+            };
+            flowChart2.setOption(flowOption2);
+
+
+
+
+            
 
 
 
@@ -151,7 +278,18 @@ export default {
 <style scoped>
     .flowblock {
         width: 100%;
-        height: 300px;
-        background-color: yellow;
+        height: 800px;
+        /* background-color: yellow; */
+    }
+    .flow-charts {
+        display: flex;
+        width: 100%;
+        height: 500px;
+    }
+    #echarts1,#echarts2 {
+        width: 50%;
+        height: 100%;
+       
+        margin-left: 50px;
     }
 </style>
